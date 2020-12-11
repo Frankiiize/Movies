7b1d4aa041481611283c8034964a691418067b1a
francisco jimnenez <frankiiize@gmail.com>
2020-12-11 14:01:30 -0300

    menu responsive listo

--- code/responsiveNav.js
+++ code/responsiveNav.js
@@ -1,17 +1,10 @@
+
 function myfuntion(){
     var x = document.getElementById("topnav");
     if(x.className === "sidebar_nav-container"){
-        x.className += " responsive";
+        x.className += " responsive change ";
     } else{
         x.className = "sidebar_nav-container"
     }
-}
-
-function margin(){
-    var x = document.getElementById("topnav");
-    if(x.className === "movies_container"){
-        x.className += " responsive2";
-    } else{
-        x.className = "movies_container";
-    }
+   
 }
